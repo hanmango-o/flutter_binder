936b7cb03362a37db01358c6dbd54c6f5fe89217
hanmango-o <hanmango.o@gmail.com>
2022-01-15 18:37:53 +0900

    v22.1.0/Binder 리뉴얼 및 sqflite(with path) 테스트 setup

---
+++ lib/crossroad_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Crossroad extends StatefulWidget {
+  const Crossroad({Key? key}) : super(key: key);
+
+  @override
+  _CrossroadState createState() => _CrossroadState();
+}
+
+class _CrossroadState extends State<Crossroad> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_binder/functions/route.dart';
-import 'package:flutter_binder/pages/topNavWithoutAppbar/top_nav_without_appbar_page.dart';
-import 'package:flutter_binder/widgets/buttons/default_button.dart';
+import 'package:flutter_binder/crossroad_screen.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -20,37 +18,8 @@ class MyApp extends StatelessWidget {
         ),
         primaryColor: Colors.black,
       ),
-      home: const MainFrame(),
-      getPages: [
-        GetPage(
-          name: "/topNavWithoutAppBar",
-          page: () => const TopNavWithoutAppBarPage(),
-          transition: Transition.leftToRight,
-        )
-      ],
-    );
-  }
-}
-
-class MainFrame extends StatelessWidget {
-  const MainFrame({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Flutter Binder")),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView(
-            children: [
-              DefaultButton(route: "/topNavWithoutAppBar"),
-              Container(),
-              const Text("test"),
-            ],
-          ),
-        ),
-      ),
+      home: const CrossroadScreen(),
+      getPages: [],
     );
   }
 }
